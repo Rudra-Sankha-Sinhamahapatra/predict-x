ef6e78278aba45165f6d0096ac57db28d834e5f4
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2025-08-02 00:52:49 +0530

    update db

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,6 +3,8 @@ import postgres from "postgres";
 import { config } from "./config";
 import * as schema from "./schema/schema";
 
+export * from "./schema/schema";
+
 const client = postgres(config.db as string);
 
 export const db = drizzle(client, {schema});
